5c4cfd1257458c21d779c89181f437a96522ddcd
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2023-11-17 14:54:00 -0500

    Forgot to use onload

--- AHDA_quicklink.user.js
+++ AHDA_quicklink.user.js
@@ -233,8 +233,10 @@ if (/view.php/.test(window.location.href)) {
 
 } else if (/auth.utoronto.ca/.test(window.location.href)) {
     // If we're not at /view.php but we're at auth.utoronto.ca, this is the home page for UTORauth
-    // Drop the A at the beginning to make the barcode input field work with the barcode scanners
-    $('input[name=barcode]').oninput = (e)=>{if(e.target.value == "A") e.target.value = "";};
+    $(function() {
+        // Drop the A at the beginning to make the barcode input field work with the barcode scanners
+        $('input[name=barcode]').oninput = (e)=>{if(e.target.value == "A") e.target.value = "";};
+    });
 }else if (/madmin.pl/.test(window.location.href)) {
 
 	$(function(){
