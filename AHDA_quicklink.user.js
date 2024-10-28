8499713914560fcbe14f35f9762cd765d4fe28c0
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2024-10-28 12:03:12 -0400

    Handle multiple results for single UTORid in Duo Admin

--- AHDA_quicklink.user.js
+++ AHDA_quicklink.user.js
@@ -18,7 +18,7 @@
 // @include     https://admin-832cdf07.duosecurity.com/*
 // @include     https://www.utorid.utoronto.ca/cgi-bin/utorid/acctrecoveryadmin.pl*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
-// @version     0.5.18
+// @version     0.5.19
 // @connect     admin-832cdf07.duosecurity.com
 // @grant       GM_setValue
 // @grant       GM_getValue
@@ -135,23 +135,29 @@ if (/view.php/.test(window.location.href)) {
                     return;
                 }
                 let resp = $.parseJSON(this.responseText)["response"];
+				let userKey = '';
                 if(resp['users'].length == 0){
                     $('span.utormfa-status').html("Could not find user, please look up manually: <a href='https://admin-832cdf07.duosecurity.com' target='_blank'>link</a>");
                     return;
                 }else if(resp['users'].length > 1){
-		    let users = resp['users'];
-		    let found = false;
-		    for(user in users){
-		        if(user.username == clientUtorid){
-                            GM_openInTab("https://admin-832cdf07.duosecurity.com/users/" + resp['users'][0]['key'], {'active': true});
-			    found = true;
-			    break;
-			}
-		    }
-		    if(!found) $('span.utormfa-status').html("Found multiple users, please look up manually: <a href='https://admin-832cdf07.duosecurity.com' target='_blank'>link</a>");
-                    return;
+					let users = resp['users'];
+					let found = false;
+					let foundUser = null;
+					for(var user in users){
+						if(user.username == clientUtorid){
+							foundUser = user;
+							found = true;
+							break;
+						}
+					}
+					if(!found){
+						$('span.utormfa-status').html("Found multiple users, please look up manually: <a href='https://admin-832cdf07.duosecurity.com' target='_blank'>link</a>");
+						return;
+					}
+					userKey = foundUser['key'];
                 }
-                GM_openInTab("https://admin-832cdf07.duosecurity.com/users/" + resp['users'][0]['key'], {'active': true});
+				if(userKey == '') userKey = resp['users'][0]['key']; // If we only found one, userKey won't have been set yet.
+                GM_openInTab("https://admin-832cdf07.duosecurity.com/users/" + userKey, {'active': true});
                 $('span.utormfa-status').remove();
             }});
             return;
